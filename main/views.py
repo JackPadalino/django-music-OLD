930b5296d42242273231dd8480ce006ce01005bf
JackPadalino <pada0867@gmail.com>
2023-12-30 12:00:12 -0500

    added nav bar and html files for music app

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
+def home(request):
     context = {}
     return render(request, "main/home.html", context)
--- music/views.py
+++ music/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def catalog(request):
+    context = {}
+    return render(request, "music/catalog.html", context)
+
+def upload(request):
+    context = {}
+    return render(request, "music/upload.html", context)

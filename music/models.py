284b1ba73dd6765f746670baf0ed2de666f80641
JackPadalino <pada0867@gmail.com>
2023-12-31 13:37:59 -0500

    updated models and db

--- music/models.py
+++ music/models.py
@@ -14,12 +14,12 @@ class Artist(models.Model):
 #     release_date = models.DateField()
 
 class Track(models.Model):
+    # artist_name = models.CharField(max_length=100)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
-    # album = models.ForeignKey(Album, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     genre = models.CharField(max_length=50)
     file = models.FileField(upload_to='uploads/',validators=[FileExtensionValidator(allowed_extensions=['mp3'])])
-    
+
     # release_date = models.DateField()
 
     def __str__(self):

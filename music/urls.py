560136ae077177e502072f9f4e260fc5192b32d7
JackPadalino <pada0867@gmail.com>
2023-12-31 19:22:38 -0500

    added api endpoints for Artist and Track with rest framework

--- music/urls.py
+++ music/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from . import views,api
 
 app_name="music"
 
@@ -7,5 +7,8 @@ urlpatterns = [
     path("", views.CatalogView.as_view(), name="catalog"),
     path("upload/", views.upload, name="upload"),
     path("download/<int:pk>", views.download, name="download"),
-    # path("upload-track/", views.upload_track, name="upload_track")
+    # api endpoints
+    path("api/artists", api.AllArtistsAPIView.as_view(), name="api-all-artists"),
+    path("api/tracks", api.AllTracksAPIView.as_view(), name="api-all-tracks"),
+    # path("api/questions/<int:pk>", api.SingleQuestionAPIView.as_view(), name="api-single-question")
 ]

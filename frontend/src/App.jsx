df5bad9122f70ec50c3e719536fb87fc4607f23d
JackPadalino <pada0867@gmail.com>
2024-01-01 14:50:32 -0500

    added react frontend with vite - everything working great!

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  const [artists, setArtists] = useState([]);
+  const fetchArtists = async () => {
+    const response = await axios.get("http://localhost:8000/music/api/artists");
+    console.log(response.data);
+    setArtists(response.data);
+  };
+
+  useEffect(() => {
+    fetchArtists();
+  }, []);
+
+  return (
+    <>
+      <h1>Artists</h1>
+      <ul>
+        {artists.map((artist) => (
+          <li key={artist.id}>{artist.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
